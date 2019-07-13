c207c3504f805c3a4c12b056924a4ba3fc8337e8
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2019-07-13 23:21:39 +0300

    test

--- wp-content/themes/shapely/new-search.php
+++ wp-content/themes/shapely/new-search.php
@@ -5,6 +5,35 @@ Template Name: Search
 
 get_header(); ?>
 
+<div class="search-page">
+    <h3 class="page_title">К сожалению, по запросу «посуда хорека» мы ничего не нашли</h3>
+    <div class="row">
+        <div class="search-page-text col col-sm-6">
+            <p>Возможно, вы ввели некорректный запрос. Проверьте правильность написания. Старайтесь использовать только ключевые слова.</p>
+        </div>
+    </div>
+    <div class="row search-two-cols">
+        <div class="col col-sm-6 search-categories">
+            <div class="title-30">Посмотрите в категориях</div>
+            <ul class="cats row">
+                <li class="col col-sm-6"><a href="#">Кресла</a></li>
+                <li class="col col-sm-6"><a href="#">Стулья</a></li>
+                <li class="col col-sm-6"><a href="#">Диваны</a></li>
+                <li class="col col-sm-6"><a href="#">Основания под столы</a></li>
+                <li class="col col-sm-6"><a href="#">Столешницы</a></li>
+                <li class="col col-sm-6"><a href="#">Барные стулья</a></li>
+            </ul>
+        </div>
+        <div class="col col-sm-6 contacts">
+            <div class="title-30">Мы поможем найти!</div>
+           <div class="contacts__text">
+               <a href="tel:08005443464">0 800 544 34 64</a>
+               <div class="schedule">Понедельние - пятница с 8 до 22</div>
+               <div><a href="#" class="white-button">Обратный звонок</a></div>
+           </div>
+        </div>
+    </div>
+</div>
 
 
 <?php
