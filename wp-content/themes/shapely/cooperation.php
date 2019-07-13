98df4df4640c5c04bf63cca5e8d12458a08779b7
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2019-07-14 00:51:02 +0300

    ooperaton page part

---
+++ wp-content/themes/shapely/cooperation.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Cooperation
+*/
+
+get_header(); ?>
+
+<div class="cooperation-page">
+    <div id="categories-wr">
+        <div class="row">
+            <div class="term-item-1 col-md-5 col-lg-5 col-sm-6 col-xs-6  col-6 col-md-offset-1 col-lg-offset-1 col-sm-offset-0 no-marg big-title">
+                <h3 class="page_title">Сотрудничество</h3>
+                <p class="text16">
+                    Мы заинтересованы в долгосрочном и надежном партнерстве с рестораторами, девелоперами и дизайнерами
+                </p>
+                <button>Оставить заявку</button>
+            </div>
+            <div class="term-item term-item-2 col-md-3 col-lg-3 col-sm-4 col-xs-6  col-6  col-md-offset-1 col-lg-offset-1  col-sm-offset-0  small-title">
+                <a href="http://decido/zavedenie/dlya-detskih-kafe/">
+                    <img src="http://decido/wp-content/uploads/2018/11/Kids-cafe-beige.svg" alt="Рестораторам">
+                    <div class="text-ww">
+                        <h3>Рестораторам</h3>
+                        <span>Получите лучший сервис, а так же наиболее выгодные цены на рынке</span>
+                    </div>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
