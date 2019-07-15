8803dc80a1138d0a25ca6ca1029a75edd7c9d45d
VasilenkoVyacheslav <vasilenko.vyacheslav@gmail.com>
2019-07-15 10:55:51 +0300

    callback form

--- wp-content/themes/shapely/cooperation.php
+++ wp-content/themes/shapely/cooperation.php
@@ -8,14 +8,14 @@ get_header(); ?>
 <div class="cooperation-page">
     <div id="categories-wr">
         <div class="row">
-            <div class="term-item-1 col-md-5 col-lg-5 col-sm-6 col-xs-6  col-6 col-md-offset-1 col-lg-offset-1 col-sm-offset-0 no-marg big-title">
+            <div class="term-item-1 col-md-5 col-lg-5 col-sm-6 col-xs-12  col-6 no-marg">
                 <h3 class="page_title">Сотрудничество</h3>
                 <p class="text16">
                     Мы заинтересованы в долгосрочном и надежном партнерстве с рестораторами, девелоперами и дизайнерами
                 </p>
                 <button>Оставить заявку</button>
             </div>
-            <div class="term-item term-item-2 col-md-3 col-lg-3 col-sm-4 col-xs-6  col-6  col-md-offset-1 col-lg-offset-1  col-sm-offset-0  small-title">
+            <div class="term-item term-item-2 col-md-3 col-lg-3 col-sm-4 col-xs-6  col-6 col-xs-offset-6  col-md-offset-1 col-lg-offset-1  col-sm-offset-0  small-title">
                 <a href="http://decido/zavedenie/dlya-detskih-kafe/">
                     <img src="http://decido/wp-content/uploads/2018/11/Kids-cafe-beige.svg" alt="Рестораторам">
                     <div class="text-ww">
@@ -24,7 +24,26 @@ get_header(); ?>
                     </div>
                 </a>
             </div>
-
+        </div>
+        <div class="row">
+            <div class="term-item term-item-3 col-md-4 col-lg-4 col-sm-5 col-xs-6 col-6 col-md-offset-1 col-lg-offset-1  col-sm-offset-0 midle-title">
+                <a href="http://decido/zavedenie/dlya-barov/">
+                    <img src="http://decido/wp-content/uploads/2018/11/Bar-beige.svg" alt="Для баров">
+                    <div class="text-ww">
+                        <h3>Девелоперам</h3>
+                        <span>Специальные цены и лояльные условия оплаты для девелоперов</span>
+                    </div>
+                </a>
+            </div>
+            <div class="term-item term-item-4 col-md-3 col-lg-3 col-sm-4 col-xs-6  col-6 col-xs-offset-6 col-md-offset-1 col-lg-offset-1  col-sm-offset-1  small-title">
+                <a href="http://decido/zavedenie/mebel-dlya-restoranov/">
+                    <img src="http://decido/wp-content/uploads/2018/11/Restaurants-beige.svg" alt="Для ресторанов">
+                    <div class="text-ww">
+                        <h3>Архитекторам</h3>
+                        <span>Лучшие условия сотрудничества и все необходимые материалы при работе над коммерческими проектами</span>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
---
+++ wp-content/themes/shapely/form-flowers.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: form-flowers
+*/
+
+get_header(); ?>
+
+<div class="text-flowers">
+    <div class="container">
+        <div class="row">
+            <div class="col-10 col-sm-6">
+                <h4 class="title-30">Давайте развивать индустрию гостеепреимства вместе</h4>
+                <p class="text16">
+                    Мы строим систему сервиса для наших клиентов и партнеров.
+                    В основе создания Decido заложено редкое сочетание профессиональных компетенций и знаний – десятилетнего опыта
+                    в изготовления мебели и пятнадцатилетнего практического опыта работы в индустрии гостеприимства. Когда мы создаем новый продукт, мы прекрасно понимаем какой формат заведения будет использовать эту модель и уделяем максимальное внимание необходимым характеристикам.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="callback-form">
+    <div class="container">
+        <div class="title-30">
+            Обратная связь
+        </div>
+        <p class="text16">Заполните форму обратной связи ниже и мы вам перезвоним</p>
+        <form class="row" id="callback">
+            <div class="col-xs-5 col-sm-3">
+                <input type="text" name="name" placeholder="Ваше имя *" required="required">
+            </div>
+            <div class="col-xs-5 col-sm-3">
+                <input type="tel" name="phone" placeholder="Номер телефона *" required="required">
+            </div>
+            <div class="col-xs-5 col-sm-3"><button type="submit" class="button" name="send_callback" value="связаться со мной">связаться со мной</div>
+
+
+
+        </form>
+    </div>
+</div>
+
+
+<?php  get_footer(); ?>
